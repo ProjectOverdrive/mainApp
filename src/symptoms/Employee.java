3ce2446c7ad4d787e52a28a685ee90a7136e1c9d
Colten Cross <colten@coltencross.com>
2017-04-12 13:44:39 -0400

    Fixed Customer, Employee, and Manager classes

--- src/symptoms/Employee.java
+++ src/symptoms/Employee.java
@@ -1,5 +1,40 @@
 /**
  * Created by Colten on 4/11/17.
  */
-public class Employee {
+
+public class Employee extends Customer {
+
+    private static int employeeID;
+    private int hourlyPay;
+
+
+    public Employee() {
+        // MUST STAY EMPTY
+        // Needed by Ormlite to save object to database
+    }
+
+    public Employee(String firstName, String lastName, String phoneNumber, String streetAddress,
+                    String city, String state, String zipcode, String email, int hourlyPay) {
+
+        super(firstName, lastName, phoneNumber, streetAddress, city, state, zipcode, email);
+        this.hourlyPay = hourlyPay;
+        employeeID += 1;
+
+    }
+
+    public static int getEmployeeID() {
+        return employeeID;
+    }
+
+    public static void setEmployeeID(int employeeID) {
+        Employee.employeeID = employeeID;
+    }
+
+    public int getHourlyPay() {
+        return hourlyPay;
+    }
+
+    public void setHourlyPay(int hourlyPay) {
+        this.hourlyPay = hourlyPay;
+    }
 }
--- src/symptoms/Manager.java
+++ src/symptoms/Manager.java
@@ -1,5 +1,29 @@
 /**
  * Created by Colten on 4/12/17.
  */
-public class Manager {
+public class Manager extends Employee {
+
+    private int managerID;
+
+    public Manager() {
+        // MUST STAY EMPTY
+        // Needed by Ormlite to save object to database
+    }
+
+    public Manager(String firstName, String lastName, String phoneNumber, String streetAddress,
+                   String city, String state, String zipcode, String email, int hourlyPay) {
+        super(firstName, lastName, phoneNumber, streetAddress, city, state, zipcode, email, hourlyPay);
+
+        managerID += 1;
+
+
+    }
+
+    public int getManagerID() {
+        return managerID;
+    }
+
+    public void setManagerID(int managerID) {
+        this.managerID = managerID;
+    }
 }
